3f261c72eff82b62588452011d1d921462a2ea03
oconely <and.ribaltovsky@gmail.com>
2018-03-01 15:47:18 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    var count = 0;
+	for (var i = 2; i <= number; i++) {
+		count += factfive(i);
+	}
+	return count;
+}
+
+function factfive(i) {
+	var count = 0;
+	while (i % 5 == 0) {
+		count++;
+		i /= 5;
+	}
+	return count;
 }
